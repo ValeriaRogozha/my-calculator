3b8fcdcbbf695fb5d6aea7bc5ae957a682650891
ValeriaRogozha <111849335+ValeriaRogozha@users.noreply.github.com>
2022-10-04 14:20:03 +0300

    push

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@
 import './App.css';
 import Wrapper from './Components/Wrapper';
 import Screen from './Components/Screen';
-import ButtonBox from './Components/ButtonBox'
-function App() {
-  return ( <Wrapper>
+import ButtonBox from './Components/ButtonBox';
+import Button from './Components/Button';
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+const App = () => {
+  return ( 
+  <Wrapper>
     <Screen value="0" />
-    <ButtonBox></ButtonBox>
+    <ButtonBox>
+      {
+        btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+            key={i}
+            className={btn === "=" ? "equals" : ""}
+            value={btn}
+            onClick={() => {
+              console.log(`${btn} clicked!`);
+            }}
+          />
+          );
+        })
+      }
+    
+        </ButtonBox>
   </Wrapper>
    
   );
