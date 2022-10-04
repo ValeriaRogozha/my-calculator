cf47b90b04ccb52c052fe11af29858f08d50f5a8
ValeriaRogozha <111849335+ValeriaRogozha@users.noreply.github.com>
2022-10-04 13:35:56 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Wrapper from './Components/Wrapper';
+import Screen from './Components/Screen';
+import ButtonBox from './Components/ButtonBox'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <Wrapper>
+    <Screen value="0" />
+    <ButtonBox></ButtonBox>
+  </Wrapper>
+   
   );
 }
 
